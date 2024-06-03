9423d1914b441240e987c3bad180bab3475ce025
Reidar Nilsen <4reidar@gmail.com>
2024-06-03 13:53:05 +0200

    Add project files.

---
+++ SingletonLog/Logger.cs
@@ -0,0 +1,37 @@
+﻿namespace SingletonLog
+{
+    // Implementerar en singleton för bara en instans
+    public class Logger
+    {
+        // privat statisk variable som håller en logger instans
+        private static volatile Logger instance;
+        private static readonly object lockObject = new object();
+        private Logger() { }
+        public static Logger Instance
+        {
+            get
+            {
+                // Första kontrollen av instansen utan lås för att förbättra prestanda
+                if (instance == null)
+                {
+                    // Låsning av kritisk sektion för att säkerställa trådsäkerhet
+                    lock (lockObject)
+                    {
+                        // Andra kontrollen av instansen inom lås för att säkerställa att endast en instans skapas
+                        if (instance == null)
+                        {
+                            instance = new Logger();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+        // publik metod för att logga till konsollen
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now}] {message}");
+        }
+
+    }
+}
---
+++ SingletonLog/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SingletonLog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = Logger.Instance;
+            logger.Log("Programmet är i gång");
+            DoSomething();
+            DoSomethingElse();
+            logger.Log("Programmet har stoppats");
+
+        }
+        static void DoSomething()
+        {
+            // Hämtar singleton-instansen av Logger och loggar ett meddelande om att något görs
+            Logger logger = Logger.Instance;
+            logger.Log("Doing something...");
+        }
+
+        // En annan metod som representerar en annan verksamhet i programmet och loggar ett meddelande
+        static void DoSomethingElse()
+        {
+            // Hämtar singleton-instansen av Logger och loggar ett meddelande om att något annat görs
+            Logger logger = Logger.Instance;
+            logger.Log("Doing something else...");
+        }
+    }
+}
